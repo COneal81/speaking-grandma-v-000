2a9dd13a0d9d9cff0570a818a8750e80ed105de4
Kari O'Neal <triadfirewood@gmail.com>
2018-07-25 11:41:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(hi)
     return "HUH?! SPEAK UP, SONNY!"
   elsif hi.uppercase
   return "NO, NOT SINCE 1938!"
-else "I LOVE YOU GRANDMA!"
+else hi =="I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
 end
