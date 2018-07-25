646475f950f0f25e9b5b1e23e87bd702d08788da
Kari O'Neal <triadfirewood@gmail.com>
2018-07-25 12:10:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(hi)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif hi == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif hi == "Hi Nana, how are you?"
+  elsif hi == "Hi!?"
     return "HUH?! SPEAK UP, SONNY!"
   elsif hi = "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
